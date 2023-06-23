9926ea7cae2c326fa426980818a234cd184f7bb1
devn0n <sochi98.sn@gmail.com>
2023-06-23 11:34:03 +0100

    Chapter 1 to 3 so far...

---
+++ chapter1/main.go
@@ -0,0 +1,14 @@
+// A Hello World Program
+
+// 
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Osoraa"
+	var greet string = "Hello"
+
+	fmt.Println(greet, "World from Introduction to Go by", name)
+}
---
+++ chapter2/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("3 + 4 =", 3.0 + 4.0)
+	fmt.Println("Hello"[3])
+	fmt.Println("Hello, " + "World")
+}
