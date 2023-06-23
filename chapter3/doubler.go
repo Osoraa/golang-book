458dbff831e97041c748725b3f2820774858c88f
devn0n <sochi98.sn@gmail.com>
2023-06-23 15:03:34 +0100

    Taking input in Go

--- chapter3/doubler.go
+++ chapter3/doubler.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Enter a number: ")
+	// Collects a number as input and doubles it. No type checking yet!!
+
 	var num rune
+
+	fmt.Println("Enter a number: ")
 	fmt.Scanf("%d", &num)
 	fmt.Println(num * 2)
 }
