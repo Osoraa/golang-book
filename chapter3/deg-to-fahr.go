00ca0947ecfacc1b70409df669fcf5d876496358
devn0n <sochi98.sn@gmail.com>
2023-06-29 08:31:39 +0100

    Implemented temp conversion logic - yet to understand function parameneters

--- chapter3/deg-to-fahr.go
+++ chapter3/deg-to-fahr.go
@@ -7,16 +7,7 @@ var (
 	choice byte
 )
 
-func main() {
-	// User makes a choice
-	fmt.Println("Make a choice\n0. Deg to Fahr\n1.Fahr to Deg?")
-	fmt.Scanf("%d", &choice)
-
-	// Requests temperature from User
-	fmt.Scanf("%f", &temp)
-}
-
-func deg_to_fahr(deg) {
+func deg_to_fahr(deg float64) float64 {
 	// Converts degrees to fahr
 
 	fahr := ((deg * 9) / 5) + 32
@@ -24,10 +15,19 @@ func deg_to_fahr(deg) {
 	fmt.Println(deg, "degree(s) is", fahr, "in Fahrenheit")
 }
 
-func fahr_to_deg(fahr) {
+func fahr_to_deg(fahr float64) float64{
 	// Converts fahr to degrees
 
 	deg := ((fahr - 32) * 5) / 9
 
 	fmt.Println(fahr, "Fahrenheit(s) is", deg, "in Fahrenheit")
+}
+
+func main() {
+	// User makes a choice
+	fmt.Println("Make a choice\n0. Deg to Fahr\n1.Fahr to Deg?")
+	fmt.Scanf("%d", &choice)
+
+	// Requests temperature from User
+	fmt.Scanf("%f", &temp)
 }
