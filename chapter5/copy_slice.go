54ec704ded572cc5555e19db5893cd622f25937d
devn0n <sochi98.sn@gmail.com>
2023-07-02 10:59:54 +0100

    Copying slices in Go

--- chapter5/copy_slice.go
+++ chapter5/copy_slice.go
@@ -14,4 +14,15 @@ func main() {
 	// Only the first two elements from 0 are copied into 1
 	fmt.Println("Slice0 is", slice0, "with length", len(slice0))
 	fmt.Println("Slice1 is", slice1, "with length", len(slice1))
+
+	// Create and copy slices
+	slice2 := []int{1, 2, 3}
+	slice3 := make([]int, 4)
+
+	// Remember, destination slice comes before source slice
+	copy(slice3, slice2)
+
+	// All elements are copied from 2 into 3
+	fmt.Println("Slice2 is", slice2, "with length", len(slice2))
+	fmt.Println("Slice3 is", slice3, "with length", len(slice3))
 }
