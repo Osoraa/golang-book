71b9320a024ce7d524eeb3ce12f1e9acf08e66c9
devn0n <sochi98.sn@gmail.com>
2023-07-04 23:42:50 +0100

    Added Return statements for d-f and f-d

--- chapter3/deg-to-fahr.go
+++ chapter3/deg-to-fahr.go
@@ -12,7 +12,9 @@ func deg_to_fahr(deg float64) float64 {
 
 	fahr := ((deg * 9) / 5) + 32
 
-	fmt.Println(deg, "degree(s) is", fahr, "in Fahrenheit")
+	// fmt.Println(deg, "degree(s) is", fahr, "in Fahrenheit")
+
+	return fahr
 }
 
 func fahr_to_deg(fahr float64) float64{
@@ -20,12 +22,14 @@ func fahr_to_deg(fahr float64) float64{
 
 	deg := ((fahr - 32) * 5) / 9
 
-	fmt.Println(fahr, "Fahrenheit(s) is", deg, "in Fahrenheit")
+	// fmt.Println(fahr, "Fahrenheit(s) is", deg, "in Fahrenheit")
+
+	return deg
 }
 
 func main() {
 	// User makes a choice
-	fmt.Println("Make a choice\n0. Deg to Fahr\n1.Fahr to Deg?")
+	fmt.Println("Make a choice\n0. Deg to Fahr\n1. Fahr to Deg?")
 	fmt.Scanf("%d", &choice)
 
 	// Requests temperature from User
