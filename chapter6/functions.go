cad55162d65c2d4087ad7f40082b5b5f3c8a559d
devn0n <sochi98.sn@gmail.com>
2023-07-05 10:12:40 +0100

    Move averaging logic into avg func

--- chapter6/functions.go
+++ chapter6/functions.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
-func () {
-	slic := {82, 83, 84, 85, 80}
-
+func avg(slx []float64) float64 {
+	// panic("Not yet Implemented!!")
 	total := 0.0
-	for _, value := range slic; {
+
+	for _, value := range slx {
 		total += value
 	}
 
-	fmt.Println(total / float64(len(slic)))
+	return total / float64(len(slx))
+}
+
+func main() {
+	slic := []float64{82, 83, 84, 85, 80}
+
+	fmt.Println(avg(slic))
 }
