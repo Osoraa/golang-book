59574b598f9dd4ed09013a607293188457e67afa
devn0n <sochi98.sn@gmail.com>
2023-07-05 02:50:47 +0100

    Temp Conversion V1

--- chapter3/temp_conversion.go
+++ chapter3/temp_conversion.go
@@ -13,7 +13,6 @@ func deg_to_fahr(deg float64) float64 {
 	fahr := ((deg * 9) / 5) + 32
 
 	// fmt.Println(deg, "degree(s) is", fahr, "in Fahrenheit")
-
 	return fahr
 }
 
@@ -23,26 +22,29 @@ func fahr_to_deg(fahr float64) float64{
 	deg := ((fahr - 32) * 5) / 9
 
 	// fmt.Println(fahr, "Fahrenheit(s) is", deg, "in Fahrenheit")
-
 	return deg
 }
 
 func main() {
-	// User makes a choice
+	// Declare Result variable
 	res := 0.0
 
-	fmt.Println("Make a choice\n0. Deg to Fahr\n1. Fahr to Deg?")
+	// Request type of conversion
+	fmt.Println("Make a choice...\n0. Deg to Fahr\n1. Fahr to Deg?")
 	fmt.Scanf("%d", &choice)
-	
+
+	// Choose which scope
 	if choice == 0 {
 		fmt.Println("Input the temperature to convert to fahrenheit?")
 		fmt.Scanf("%f", &temp)
 
+		// Call degree converter
 		res = deg_to_fahr(temp)
 	} else {
 		fmt.Println("Input the temperature to convert to degrees?")
 		fmt.Scanf("%f", &temp)
 
+		// Call Fahrenheit converter
 		res = fahr_to_deg(temp)
 	}
 
