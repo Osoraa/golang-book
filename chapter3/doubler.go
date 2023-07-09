83fd97ef01b9c5f4120e66c6e79d1279e350db3e
devn0n <sochi98.sn@gmail.com>
2023-07-09 23:53:48 +0100

    Using new keyword to create input variables

--- chapter3/doubler.go
+++ chapter3/doubler.go
@@ -6,8 +6,13 @@ func main() {
 	// Collects a number as input and doubles it. No type checking yet!!
 
 	var num rune
+	// num := new(rune)
 
 	fmt.Println("Enter a number: ")
+
 	fmt.Scanf("%d", &num)
+	// fmt.Scanf("%d", num)
+
 	fmt.Println(num * 2)
+	// fmt.Println(*num * 2)
 }
