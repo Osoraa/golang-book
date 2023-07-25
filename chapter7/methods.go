f992246920096b978afea6cc346a51dd48f0e732
devn0n <sochi98.sn@gmail.com>
2023-07-25 01:21:05 +0100

    Creating methods

---
+++ chapter7/methods.go
@@ -0,0 +1,23 @@
+package main
+
+// Demonstrates methods in Go.
+
+import ("fmt")
+
+type Rectangle struct {
+	l, b int
+}
+
+func (rec *Rectangle) area() int {
+	return rec.l * rec.b
+}
+
+func main() {
+	rec := new(Rectangle)
+	rec.l, rec.b = 10, 5
+
+	rect := Rectangle{l: 20, b: 10}
+
+	fmt.Println(rec.area())
+	fmt.Println(rect.area())
+}
