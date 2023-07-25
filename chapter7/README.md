df841108e8e25803fc8ecbb3581955a18cedd086
devn0n <sochi98.sn@gmail.com>
2023-07-25 01:20:18 +0100

    Creating a Struct

---
+++ chapter7/README.md
@@ -0,0 +1,6 @@
+# Introduction to GO by Caleb Doxsey
+
+## Chapter 7: Structs and Interfaces
+
+### Key Takeaways
+- 
---
+++ chapter7/struct_init.go
@@ -0,0 +1,39 @@
+package main
+
+// Demonstrating Structs in Go
+
+import ("fmt"; "math")
+
+// Declaring a Struct
+type Circle struct {
+	x, y, r float64
+}
+
+func circleArea(c *Circle) float64 {
+	// Calculates the area of a circle
+	return math.Pi * c.r * c.r
+}
+
+func main() {
+	// Instantiating a Struct
+	var circ Circle
+	circl := new(Circle)
+	cir := Circle{x: 5, y: 5, r: 2}
+	cirl := &Circle{4, 4, 1}
+
+	// Setting Struct values
+	circ.x = 10
+	circ.y = 11
+	circ.r = 5
+
+	circl.x = 20
+	circl.y = 21
+	circl.r = 10
+
+	fmt.Println(circ)
+	fmt.Println(*circl) // Dereferenced pointer
+	fmt.Println(cir)
+	fmt.Println(*cirl)
+
+	fmt.Println(circleArea(&circ))
+}
