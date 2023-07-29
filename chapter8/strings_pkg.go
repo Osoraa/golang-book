e80ede0b12e590dd62f63d9d3af171b374869c29
devn0n <sochi98.sn@gmail.com>
2023-07-29 19:10:08 +0100

    strings package - Index and Join

--- chapter8/strings_pkg.go
+++ chapter8/strings_pkg.go
@@ -12,16 +12,25 @@ func main() {
 
 	// strings.Count
 	fmt.Println(strings.Count("Hippopotamus", "p"))
-	
+
 	// strings.HasPrefix
 	fmt.Println(strings.HasPrefix("Hippopotamus", "Hip"))
 
 	// strings.HasPrefix
 	fmt.Println(strings.HasPrefix("Hippopotamus", "Hp"))
-	
+
 	// strings.HasSuffix
 	fmt.Println(strings.HasSuffix("Hippopotamus", "mus"))
 
 	// strings.HasSuffix
 	fmt.Println(strings.HasSuffix("Hippopotamus", "ms"))
+
+	// strings.Index
+	fmt.Println(strings.Index("Hippopotamus", "mus"))
+
+	// strings.Index
+	fmt.Println(strings.Index("Hippopotamus", "z"))	// -1
+
+	// strings.Join
+	fmt.Println(strings.Join([]string{"Hippopotamus", "mus"}, "-lmao-"))
 }
